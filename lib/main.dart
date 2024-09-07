8ca87003c0fe7f612b5b92bcd14a34d88627853f
Renan <renancvr777@gmail.com>
2024-09-06 21:50:25 -0300

    fix: class MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Text(
-      "oi",
-      textDirection: TextDirection.ltr,
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Oi',
+      home: Container(
+        color: const Color.fromARGB(255, 221, 9, 9),
+      ),
+    );
+  }
 }
