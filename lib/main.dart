471a6e8a4205d612aafb29c282d96e60fcbbffef
renanjava <renancvr777@gmail.com>
2024-09-13 16:50:16 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home',
-      home: HomeScreen(),
+      title: 'Gerenciamento de pedidos pendentes',
+      home: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UsuarioScreen()),
+            );
+          },
+          child: Text("usuario")),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class PedidoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Gerenciamento de pedidos pendentes"),
+        title: Text("Pedidos"),
       ),
       body: Container(
         color: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CadastrarPedidoScreen()),
+          );
+        },
+        child: Text("Cadastrar pedido"),
+      ),
+    );
+  }
+}
+
+class UsuarioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Usuários"),
+      ),
+      body: Column(
+        children: [
+          TextFormField(),
+          Padding(padding: EdgeInsets.only(bottom: 20.0)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -39,33 +72,40 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class CadastrarPedidoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pedidos"),
+      ),
+      body: Container(
+        color: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CadastrarUsuarioScreen()),
+          );
+        },
+        child: Text("Cadastrar pedido"),
+      ),
+    );
+  }
+}
+
 class CadastrarUsuarioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cadastrar usuário"),
+        title: Text("Usuários"),
       ),
       body: Column(
         children: [
-          Container(
-            color: Colors.blue,
-            height: 100,
-            width: double
-                .infinity, // Use double.infinity to make the container stretch across the screen
-          ),
+          TextFormField(),
           Padding(padding: EdgeInsets.only(bottom: 20.0)),
-          Container(
-            color: Colors.blue,
-            height: 100,
-            width: double.infinity, // Same as above
-          ),
-          Padding(padding: EdgeInsets.only(bottom: 20.0)),
-          Container(
-            color: Colors.blue,
-            height: 100,
-            width: double.infinity, // Same as above
-          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
